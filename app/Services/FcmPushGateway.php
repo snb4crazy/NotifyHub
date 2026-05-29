63182a730dcdcad50624a4cc659115ef80c9304c
Serhii Dymenko <serge.sergeevich@gmail.com>
2026-05-29 01:09:39 -0400

    CI/CD: fix extra lines & spaces

--- app/Services/FcmPushGateway.php
+++ app/Services/FcmPushGateway.php
@@ -13,8 +13,7 @@ class FcmPushGateway implements PushGateway
     public function __construct(
         protected FcmAccessTokenService $accessTokenService,
         protected MobilePushPayloadFactory $payloadFactory,
-    ) {
-    }
+    ) {}
 
     /**
      * Send a push notification to all enabled devices in the project.
@@ -54,4 +53,3 @@ class FcmPushGateway implements PushGateway
         }
     }
 }
-
